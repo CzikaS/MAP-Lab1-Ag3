1083087058785e4c8049b5342f91f2cae2d8814d
CzikaS <czikastefania@yahoo.com>
2025-10-21 09:47:14 +0300

    multiplication of two big numbers method + updated subtraction

--- src/BigNumbers.java
+++ src/BigNumbers.java
@@ -2,39 +2,50 @@ public class BigNumbers {
     public BigNumbers() {
     }
 
-    public int[] sum(int[] nr1, int[] nr2){
+    public int[] sum(int[] bigNr1, int[] bigNr2){
         int carry = 0;
-        if (nr1.length != nr2.length || nr1[0] == 0 || nr2[0] == 0)
+        if (bigNr1.length != bigNr2.length || bigNr1[0] == 0 || bigNr2[0] == 0)
             throw new Error("Length differs");
-        int[] result = new int[nr1.length+1];
-        for (int i = nr1.length-1; i>=0; i--){
-            int sum = nr1[i] + nr2[i];
+        int[] result = new int[bigNr1.length+1];
+        for (int i = bigNr1.length-1; i>=0; i--){
+            int sum = bigNr1[i] + bigNr2[i];
             result[i] = carry + sum % 10;
             sum /= 10;
             carry = sum;
         }
         if (carry != 0) {
-            for (int i = nr1.length; i > 0; i--)
+            for (int i = bigNr1.length; i > 0; i--)
                 result[i] = result[i - 1];
             result[0] = carry;
         }
         return result;
     }
 
-    public int[] sub(int[] nr1, int[] nr2){
+    public int[] sub(int[] bigNr1, int[] bigNr2){
         int borrowed = 0;
-        if (nr1.length != nr2.length || nr1[0] == 0 || nr2[0] == 0)
+        if (bigNr1.length != bigNr2.length || bigNr1[0] == 0 || bigNr2[0] == 0)
             throw new Error("Length differs");
-        int[] result = new int[nr1.length];
-        if (nr2[0] > nr1[0]) {
-            for (int i = 0; i < nr1.length; i++) {
-                int tmp = nr1[i];
-                nr1[i] = nr2[i];
-                nr2[i] = tmp;
+        int[] result = new int[bigNr1.length];
+
+        boolean swap = false;
+        for (int i = 0; i < bigNr1.length; i++) {
+            if (bigNr1[i] > bigNr2[i]) {
+                swap = false;
+                break;
+            } else if (bigNr1[i] < bigNr2[i]) {
+                swap = true;
+                break;
             }
         }
-        for (int i = nr1.length-1; i>0; i--){
-            int diff = nr1[i] - nr2[i] - borrowed;
+
+        if (swap) {
+            int[] aux = bigNr1;
+            bigNr1 = bigNr2;
+            bigNr2 = aux;
+        }
+
+        for (int i = bigNr1.length-1; i>0; i--){
+            int diff = bigNr1[i] - bigNr2[i] - borrowed;
             if (diff < 0) {
                 diff += 10;
                 borrowed = 1;
@@ -43,7 +54,43 @@ public class BigNumbers {
             }
             result[i] = diff;
         }
-        result[0] = nr1[0] - nr2[0] - borrowed;
+        result[0] = bigNr1[0] - bigNr2[0] - borrowed;
+
+        int firstNonZero = 0;
+        while (firstNonZero < result.length - 1 && result[firstNonZero] == 0) {
+            firstNonZero++;
+        }
+        if (firstNonZero != 0){
+            int[] trimmed = new int[result.length - firstNonZero];
+            int trimmedIndex = 0;
+            for (int index = firstNonZero; index < result.length; index++) {
+                trimmed[trimmedIndex] = result[index];
+                trimmedIndex++;
+            }
+            result = trimmed;
+        }
+
+        return result;
+    }
+
+    public int[] multiply(int[] bigNr, int digit) {
+        int n = bigNr.length;
+        int[] result = new int[n + 1];
+        int carry = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int prod = bigNr[i] * digit + carry;
+            result[i + 1] = prod % 10;
+            carry = prod / 10;
+        }
+
+        if (carry == 0){
+            int[] trimmed = new int[bigNr.length];
+            for (int index = 1; index <= bigNr.length; index++)
+                trimmed[index-1] =result[index];
+            result = trimmed;
+        } else
+            result[0] = carry;
 
         return result;
     }
--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,19 @@ public class Main {
     public static void main(String[] args) {
         int[] number1 = {1, 2, 3, 4, 5, 6};
         int[] number2 = {9, 0, 8, 2, 6, 4};
-        int[] nr1 = {9,9,9,9};
+        int[] nr1 = {9,9,9,6};
         int[] nr2 = {9, 9,9,9};
         BigNumbers operations = new BigNumbers();
         System.out.println(Arrays.toString(operations.sum(number1,number2)));
         System.out.println(Arrays.toString(operations.sum(nr1,nr2)));
 
         System.out.println(Arrays.toString(operations.sub(number1,number2)));
+        System.out.println(Arrays.toString(operations.sub(nr1,nr2)));
+
+        // System.out.println(Arrays.toString(operations.multiply(nr1,2)));
+        System.out.println(Arrays.toString(operations.multiply(number1,2)));
+
+
 
     }
 }
